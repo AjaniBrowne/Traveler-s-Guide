0892aa8885bdca8cb3b75f2a0a2afb11334b4a55
Alejandro Zapata <alzapata317@gmail.com>
2022-07-11 13:35:08 -0400

    added second api function to airbnb

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -8,22 +8,9 @@ var requestOptions = {
     .then(response => response.json())
     .then(result => console.log(result))
     .catch(error => console.log('error', error));
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': 'e5eb859401msh2d369e18bd60dd2p1a12f2jsn5f6a801bcc56',
-            'X-RapidAPI-Host': 'airbnb19.p.rapidapi.com'
-        }
-    };
    
-    const information = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': 'a4d6bc9b41msh312d9c1f7ef35a0p11686fjsn1be2f214b1a1',
-            'X-RapidAPI-Host': 'airbnb19.p.rapidapi.com'
-        }
-    };
-    fetch('https://airbnb19.p.rapidapi.com/api/v1/getCategory', information)
+
+/*    fetch('https://airbnb19.p.rapidapi.com/api/v1/getCategory', information)
         .then(response => response.json())
         .then(response => console.log(response))
         .catch(err => console.error(err)); 
@@ -54,7 +41,7 @@ fetch("https://api.geoapify.com/v1/geocode/search?text=38%20Upper%20Montagu%20St
         })
     }
 
-
+*/
         var savedAirbnb = localStorage.getItem("airbnb") || [];
 if (savedAirbnb.length > 0) {
     airBnb = savedAirbnb;
@@ -78,19 +65,43 @@ if (savedAirbnb.length > 0) {
             card.append(airBnbName, cityEl);
 
             var renderEl = document.getElementById("currentRender");
-            console.log(card)
+            
             renderEl.append(card);
 
 
         });
  }
- function inputcity() {
+function inputcity() {
+    const information = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'c1ce3ad0b5msh9cfdca5eeaa64ccp1932e1jsn6be0c2068f56',
+            'X-RapidAPI-Host': 'airbnb19.p.rapidapi.com'
+        }
+    };
     var initial = 'https://airbnb19.p.rapidapi.com/api/v1/searchDestination?query='
     var country = '&country=USA'
     var url = initial + place.value + country
     fetch(url, information)
-        .then(response => response.json())
-        .then(response => console.log(response))
-        .catch(err => console.error(err));
+    .then(response => response.json())
+    .then(result => {console.log(result)
+        const id = result.data[0].id
+        getProperty(id)
+    })
+    .catch(error => console.log('error', error));
+    
+}
+function getProperty(identifier) {
+    const information2 = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '3cdbe490ddmsh751dfcb32382a4fp1f05c7jsn32c79cfcfe7a',
+            'X-RapidAPI-Host': 'airbnb19.p.rapidapi.com'
+        }
+    };
+    fetch('https://airbnb19.p.rapidapi.com/api/v1/searchPropertyByPlace?id=' + identifier +'&totalRecords=10&currency=USD&adults=1', information2)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
 }
 bnbbtn.addEventListener('click', inputcity)
