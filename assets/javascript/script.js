4bb12d020034b19fa57240c998b0e29a19f196fb
AjaniBrowne <ajani.browne@yahoo.com>
2022-07-11 10:35:54 -0400

    commit on feature-12 " changed airbnb api"

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,16 +1,4 @@
 
-//   fetch("https://api.geoapify.com/v1/geocode/search?text=38%20Upper%20Montagu%20Street%2C%20Westminster%20W1H%201LJ%2C%20United%20Kingdom&apiKey=e2c64eb127544922abc9e8cd2503fbed", requestOptions)
-//     .then(response => response.json())
-//     .then(result => console.log(result))
-//     .catch(error => console.log('error', error));
-
-//     const options = {
-//         method: 'GET',
-//         headers: {
-//             'X-RapidAPI-Key': '95a31a0cb5mshec7999ec6d2f812p19a194jsnde5e50058307',
-//             'X-RapidAPI-Host': 'airbnb19.p.rapidapi.com'
-//         }
-//     };
 var requestOptions = {
     method: 'GET',
   };
@@ -85,3 +73,14 @@ if (savedAirbnb.length > 0) {
         });
  }
         
+ const information = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'a4d6bc9b41msh312d9c1f7ef35a0p11686fjsn1be2f214b1a1',
+        'X-RapidAPI-Host': 'airbnb19.p.rapidapi.com'
+    }
+};
+fetch('https://airbnb19.p.rapidapi.com/api/v1/getCategory', information)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
